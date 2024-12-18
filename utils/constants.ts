88c98ed6c083a93b1002e7c9a099a6419d9fbe4c
bsuarez- <beny.suarez_piedra@me.com>
2024-12-18 22:04:39 +0100

    oups forgot the right constant

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,8 +3,8 @@ export const MAX_AGE = 60 * 60 * 24 // 1 days
 export const SALT_ROUNDS = 10;
 export const PHONE = "+32";
 export const MAIL = "reikibeny@mail.be"
-// export const PATH = process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "" : "/reikibeny-next"
-export const PATH = process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "" : ""
+export const PATH = process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "" : "/reikibeny-next"
+// export const PATH = process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "" : ""
 export const DOMAIN_NAME = "//reikibeny.be";
 // export const DOMAIN_NAME = "https://chamasutra.com";
  
