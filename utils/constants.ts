1af6ecd4208c1574b267efeedc682e45073d1a8f
bsuarez- <beny.suarez_piedra@me.com>
2025-01-21 18:56:23 +0100

    I think the path is correct now

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,7 +3,7 @@ export const MAX_AGE = 60 * 60 * 24 // 1 days
 export const SALT_ROUNDS = 10;
 export const PHONE = "+32";
 export const MAIL = "reikibeny@mail.be"
-export const PATH = process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "" : "/reikibeny"
+export const PATH = process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "" : ""
 // export const PATH = process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "" : ""
 export const DOMAIN_NAME = "//reikibeny.be";
 // export const DOMAIN_NAME = "https://chamasutra.com";
