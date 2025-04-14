4c688695fef6413b86643287fc04ee4711457a6b
bsuarez- <beny.suarez_piedra@me.com>
2025-04-14 17:05:33 +0200

    gpt help

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/reikibeny-next",
+  // basePath: "/reikibeny-next",
   images: { unoptimized: true },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
