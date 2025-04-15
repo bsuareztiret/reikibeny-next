49b6dcc60816aaedb3cb229830ed01f8c7463131
bsuarez- <beny.suarez_piedra@me.com>
2025-04-15 09:12:28 +0200

    cname update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   // basePath: "/reikibeny-next",
   images: { unoptimized: true },
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'reikibeny.be' }],
+        destination: 'https://www.reikibeny.be',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
